59e9c579d63ff179c44adb3bd62fd764a0306629
dassajib <sajibdas.sd@gmail.com>
2023-08-23 16:03:25 +0600

    new project added which name is playing with css variable with js

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -1,22 +1,14 @@
 function clock() {
   let hour = document.getElementById("hour");
-
   let minute = document.getElementById("minute");
-
   let second = document.getElementById("second");
-
   let currentHourTime = new Date().getHours();
-
   let currentMinuteTime = new Date().getMinutes();
-
   let curentSecondTime = new Date().getSeconds();
-
   hour.innerHTML = currentHourTime;
-
   minute.innerHTML = currentMinuteTime;
-
   second.innerHTML = curentSecondTime;
 }
 
-
+// The setInterval() method calls a function at specified intervals (in milliseconds).
 let interval = setInterval(clock, 1000);
